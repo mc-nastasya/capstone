e70438809ae5fc6f465353f1340ec6887952b0e8
Анастасия Макаренко <anastasiia@Anastasias-Air.localdomain>
2022-09-19 16:35:37 -0400

    style the header for mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,18 @@
 import "./Header.scss";
+import Logo from "../../assets/icons/logo.svg";
+import Menu from "../../assets/icons/menu.svg";
+
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="header__left">
+            <img src={Logo} alt='logo' className="header__logo"/>
+        </div>
+        <div className="header__right">
+            <img src={Menu} alt="menu-burger" className="header__burger-icon" />
+        </div>
+    </div>
   )
 }
 
