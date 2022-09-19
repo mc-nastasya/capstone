3343f84454b21d460dd57950b7b70b447e8ea0d4
Анастасия Макаренко <anastasiia@Anastasias-Air.localdomain>
2022-09-19 16:20:55 -0400

    add assets, colors, resets and fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 
 function App() {
   return (
    <BrowserRouter>
+    <Header />
     <Routes>
       <Route to="/"/>
     </Routes>
