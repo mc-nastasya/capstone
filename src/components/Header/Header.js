3aa962b1d8c614d92db9325414e7bf9d3a8dc1db
Анастасия Макаренко <anastasiia@Anastasias-Air.localdomain>
2022-09-19 16:42:42 -0400

    create component for menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,26 @@
 import "./Header.scss";
 import Logo from "../../assets/icons/logo.svg";
 import Menu from "../../assets/icons/menu.svg";
+import { useState } from "react";
 
 
 function Header() {
-  return (
-    <div className="header">
-        <div className="header__left">
-            <img src={Logo} alt='logo' className="header__logo"/>
+    const [menu, setMenu] = useState(false);
+
+    const handleClick = () => {
+        setMenu(true);
+    }
+
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src={Logo} alt='logo' onClick={handleClick} className="header__logo"/>
+            </div>
+            <div className="header__right">
+                <img src={Menu} alt="menu-burger" className="header__burger-icon" />
+            </div>
         </div>
-        <div className="header__right">
-            <img src={Menu} alt="menu-burger" className="header__burger-icon" />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Header
