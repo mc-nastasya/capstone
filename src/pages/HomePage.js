73e901a9e8b4bff98766c43496db0dc7f9c741d4
Анастасия Макаренко <anastasiia@Anastasias-Air.localdomain>
2022-09-21 14:51:28 -0400

    create mobile styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import AboutPreview from "../components/AboutPreview/AboutPreview";
+import EmailForm from "../components/EmailForm/EmailForm";
 
 function HomePage() {
   return (
     <>
         <AboutPreview />
+        <EmailForm />
     </>
   )
 }
