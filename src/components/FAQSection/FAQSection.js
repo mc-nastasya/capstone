f0c06243ceb037bcc7ce3d7e2fa713a91061ea06
Анастасия Макаренко <anastasiia@Anastasias-Air.localdomain>
2022-09-23 11:42:09 -0400

    add tablet view styling

--- src/components/FAQSection/FAQSection.js
+++ src/components/FAQSection/FAQSection.js
@@ -12,12 +12,12 @@ function FAQSection() {
         {
             id: "2",
             q: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
-            a: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+            a: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt Amet minim mollit non deserunt Amet minim mollit non deserunt Amet minim mollit non deserunt Amet minim mollit non deserunt"
         },
         {
             id: "3",
             q: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
-            a: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+            a: "What are the three most important characteristics of this function? How would you stack rank yourself from strongest to least developed among these traits?"
         }
     ]
   return (
