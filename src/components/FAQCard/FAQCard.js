25254b13a9926eee80ba0a0a25e8fbc3448e5359
Анастасия Макаренко <anastasiia@Anastasias-Air.localdomain>
2022-09-27 10:53:23 -0400

    add axios rquest for faq

--- src/components/FAQCard/FAQCard.js
+++ src/components/FAQCard/FAQCard.js
@@ -5,11 +5,11 @@ function FAQCard({pair, i}) {
     <div className="faq-card">
         <div className="faq-card__section">
             <h3 className="faq-card__title faq-card__question">{`Q${i+1}`}</h3>
-            <p className="faq-card__text">{pair.q}</p>
+            <p className="faq-card__text">{pair.question}</p>
         </div>
         <div className="faq-card__section">
             <h3 className="faq-card__title faq-card__answer">{`A${i+1}`}</h3>
-            <p className="faq-card__text">{pair.a}</p>
+            <p className="faq-card__text">{pair.answer}</p>
         </div>
     </div>
   )
--- src/components/FAQSection/FAQSection.js
+++ src/components/FAQSection/FAQSection.js
@@ -1,33 +1,33 @@
 import "./FAQSection.scss";
 import FAQCard from '../FAQCard/FAQCard';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function FAQSection() {
 
-    const faq = [
-        {
-            id: "1",
-            q: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
-            a: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-        },
-        {
-            id: "2",
-            q: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
-            a: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt Amet minim mollit non deserunt Amet minim mollit non deserunt Amet minim mollit non deserunt Amet minim mollit non deserunt"
-        },
-        {
-            id: "3",
-            q: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
-            a: "What are the three most important characteristics of this function? How would you stack rank yourself from strongest to least developed among these traits?"
-        }
-    ]
-  return (
-    <div className="faq">
-        <h1 className="faq__title">Frequently Asked Questions to Eva</h1>
-        {faq.map((pair , i)=>{
-            return <FAQCard key={pair.id} i={i} pair={pair} />
-        })}
-    </div>
-  )
+    const [faq, setFaq] = useState();
+
+    useEffect(()=>{
+        axios
+            .get(`http://localhost:8080/faq`)
+            .then((response)=>{
+                setFaq(response.data);
+            })
+    }, [])
+
+    if(!faq) {
+        return <p>Loading...</p>
+    }
+
+
+    return (
+        <div className="faq">
+            <h1 className="faq__title">Frequently Asked Questions to Eva</h1>
+            {faq.map((pair , i)=>{
+                return <FAQCard key={pair.id} i={i} pair={pair} />
+            })}
+        </div>
+    )
 }
 
 export default FAQSection
