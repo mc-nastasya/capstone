43eb0747d76d5c420161d4d012d5a819b2206be8
Анастасия Макаренко <anastasiia@Anastasias-MacBook-Air.local>
2022-09-26 22:58:09 -0400

    add email form validation

--- src/components/EmailForm/EmailForm.js
+++ src/components/EmailForm/EmailForm.js
@@ -1,30 +1,68 @@
 import './EmailForm.scss';
 import HedgehogMob from '../../assets/images/hedgehog.png';
 import HedgehogNotMob from '../../assets/images/hedgehog-notmob.png';
+import { useState } from 'react';
+import axios from 'axios';
 
 function EmailForm() {
-  return (
-    <div className='email-form'>
-        <div className='email-form__top'></div>
-        <div className='email-form__bottom-background'>
-            <div className='email-form__bottom'>
-                <img src={HedgehogNotMob} alt='hedgehog' className='email-form__image-notmobile' />
-                <h1 className='email-form__title'> Get book news first</h1>
-                <img src={HedgehogMob} alt='hedgehog' className='email-form__image-mobile'/>
-                <p className='email-form__description'>
-                    Book announcements, different literature events 
-                    and programs, sales for courses, TOP book 
-                    lists and much more! No spam :)
-                </p>
-                <form className='email-form__form'>
-                    <label className='email-form__label'> Your email</label>
-                    <input className='email-form__input' type="email" placeholder="my_email@example.com" />
-                    <button className='email-form__button'>Subscribe</button>
-                </form>
+
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [unsuccess, setUnsuccess] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setError(false);
+
+        const email = event.target.email.value;
+
+        if(!email || !email.includes("@")) {
+            setError(true);
+        }
+
+        const newEmail = {
+            email: email
+        };
+
+        axios
+            .post('http://localhost:8080/email', newEmail)
+            .then(()=>{
+                setSuccess(true);
+                event.target.email.value = "";
+            })
+            .catch((error)=>{
+                console.log(error);
+                setUnsuccess(true);
+            })
+    }
+
+    const handleClick = ()=> {
+        setError(false);
+    }
+
+    return (
+        <div className='email-form'>
+            <div className='email-form__top'></div>
+            <div className='email-form__bottom-background'>
+                <div className='email-form__bottom'>
+                    <img src={HedgehogNotMob} alt='hedgehog' className='email-form__image-notmobile' />
+                    <h1 className='email-form__title'> Get book news first</h1>
+                    <img src={HedgehogMob} alt='hedgehog' className='email-form__image-mobile'/>
+                    <p className='email-form__description'>
+                        Book announcements, different literature events 
+                        and programs, sales for courses, TOP book 
+                        lists and much more! No spam :)
+                    </p>
+                    <form className='email-form__form'  onSubmit={handleSubmit}>
+                        <label className='email-form__label'> Your email*</label>
+                        <input className={`email-form__input ${error? "email-form__error-state" : null}`} name='email' type="text" placeholder="my_email@example.com" onClick={handleClick}/>
+                        {error? <p className='email-form__error'>Enter valid email</p> : null}
+                        <button type='submit' className='email-form__button'>Subscribe</button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default EmailForm
+export default EmailForm;
